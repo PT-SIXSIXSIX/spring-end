3aa8dece18f9250f86680496071063f5cf1a4002
RunningPhoton <runningphoton@RunningPhotondeMacBook-Pro.local>
2018-03-10 13:46:37 +0800

    实现拦截器

--- PTsix/src/main/java/com/PT/tools/TokenInterceptor.java
+++ PTsix/src/main/java/com/PT/tools/TokenInterceptor.java
@@ -3,53 +3,40 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import com.PT.entity.User;
-import com.PT.tools.ResponseData;
 public class TokenInterceptor implements HandlerInterceptor{
+    @Override
     public void afterCompletion(HttpServletRequest request,
                                 HttpServletResponse response, Object handler, Exception arg3)
             throws Exception {
     }
+    @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response,
                            Object handler, ModelAndView model) throws Exception {
     }
     //拦截每个请求
+    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
         response.setCharacterEncoding("utf-8");
-        String token = request.getParameter("accessToken");
+        String userId = request.getHeader("userId");
+        String token = request.getHeader("accessToken");
+//        System.out.println("ID: "+userId+"  token: "+token);
+        String sign = TokenOptions.getKey(TokenOptions.TOKEN_PREFIX+userId);
         ResponseData responseData = ResponseData.ok();
-        //token不存在
-        if(null != token) {
-            /**
-             * debug待改
-             */
-//            int id = (int) JWT.parserJavaWebToken(token).get("userId");
-            int id = 0;
-            String loginId = request.getParameter("userId");
-            //解密token后的loginId与用户传来的loginId不一致，一般都是token过期
-            if(null != loginId) {
-                if(Integer.parseInt(loginId) == id) {
-                    return true;
-                }
-                else{
-                    responseData = ResponseData.forbidden();
-                    responseMessage(response, response.getWriter(), responseData);
-                    return false;
-                }
-            }
-            else
-            {
-                responseData = ResponseData.forbidden();
-                responseMessage(response, response.getWriter(), responseData);
-                return false;
-            }
+        if(null == sign || StringUtils.isBlank(sign)) {
+            responseData.putDataValue("message", "token过期");
+            responseMessage(response, response.getWriter(), responseData);
+            return false;
         }
-        else
-        {
+        else if(StringUtils.equals(sign, token)) {
+            return true;
+        }
+        else {
             responseData = ResponseData.forbidden();
+            responseData.putDataValue("message", "token验证失败");
             responseMessage(response, response.getWriter(), responseData);
             return false;
         }
