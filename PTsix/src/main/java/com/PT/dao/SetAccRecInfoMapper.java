13196bdead432e0caa615d4bd7ed16be595d4b92
RunningPhoton <runningphoton@RunningPhotondeMacBook-Pro.local>
2018-03-10 17:24:29 +0800

    注册模块

---
+++ PTsix/src/main/java/com/PT/dao/SetAccRecInfoMapper.java
@@ -0,0 +1,8 @@
+package com.PT.dao;
+
+import com.PT.bean.SetAccRecInfoBean;
+
+import java.util.List;
+
+public interface SetAccRecInfoMapper {
+}
--- PTsix/src/main/java/com/PT/service/RegistryLogonService.java
+++ PTsix/src/main/java/com/PT/service/RegistryLogonService.java
@@ -1,5 +1,6 @@
 package com.PT.service;
 
+import com.PT.bean.StorekeeperInfoBean;
 import com.PT.entity.Store;
 import com.PT.entity.User;
 
@@ -22,4 +23,14 @@ public interface RegistryLogonService {
      * @return
      */
     public User login(String phone, String password);
+
+    /**
+     * 验证电话号是否存在
+     * @param phone
+     * @return
+     */
+    public boolean verifyPhone(String phone);
+
+    public boolean changePassword(int id, String old, String pwd);
+    public boolean changePassword(StorekeeperInfoBean storekeeperInfoBean, String pwd);
 }
