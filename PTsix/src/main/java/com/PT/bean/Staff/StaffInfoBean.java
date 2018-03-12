e8e25ff1b84cd954014934b5de21ddbf4b0a885b
Chen-Dixi <cdxcqu2014@163.com>
2018-03-12 16:04:27 +0800

    改正订单模块，店员管理，增加订单信息视图

---
+++ PTsix/src/main/java/com/PT/bean/Staff/StaffInfoBean.java
@@ -0,0 +1,31 @@
+package com.PT.bean.Staff;
+
+public class StaffInfoBean {
+    private int staff_id;
+    private String name;
+    private String phone;
+
+    public int getStaff_id() {
+        return staff_id;
+    }
+
+    public void setStaff_id(int staff_id) {
+        this.staff_id = staff_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
