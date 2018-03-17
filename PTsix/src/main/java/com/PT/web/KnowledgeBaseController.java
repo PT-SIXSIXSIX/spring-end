b3ce6818ac955423efbfeee8e2d09c6637bc1abc
Chen-Dixi <cdxcqu2014@163.com>
2018-03-17 19:02:31 +0800

    aa

---
+++ PTsix/src/main/java/com/PT/web/KnowledgeBaseController.java
@@ -0,0 +1,13 @@
+package com.PT.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/users/{user_id}")
+public class KnowledgeBaseController {
+
+
+
+}
